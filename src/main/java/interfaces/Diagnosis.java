237ca3aa8cfc1290351af5e09dd7d6bb4d971206
Brian Omondi <omondibrian392@gmail.com>
2021-11-07 12:27:52 +0300

    added interfaces for  DTO's

---
+++ src/main/java/interfaces/Diagnosis.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package interfaces;
+import java.util.ArrayList;
+/**
+ *
+ * @author brian
+ */
+public class Diagnosis {
+    User user;
+    String consultation;
+    ArrayList<Lab> lab;
+    String notes;
+    String prescription;
+    
+}
+
+
---
+++ src/main/java/interfaces/StudentQueue.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package interfaces;
+
+/**
+ *
+ * @author brian
+ */
+public class StudentQueue {
+    public String RegNo;
+    public String Intent;
+    public String Date;
+}
