b8f0c107767828f6e798ae46b21c34d7b8860fbe
ojayjonathan <jonathanonderi2018@yahoo.com>
2022-01-05 13:39:27 +0300

    update

--- src/main/java/utils/Utils.java
+++ src/main/java/utils/Utils.java
@@ -2,8 +2,7 @@ package utils;
 
 public class Utils {
 
-    public static String databasePath = "/home/ojay/dev/web/COMP-341-Web-With-JSP-Servlets/src/main/webapp/META-INF/database/";
-
+    public static String databasePath = "/home/ojay/dev/web/COMP-341-Web-With-JSP-Servlets/src/main/database/";
     public static String iDGenerator() {
         String id = "";
         for (int i = 0; i < 10; ++i) {
